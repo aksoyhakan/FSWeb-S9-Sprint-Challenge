21a074a8dfb1bf5f56a9aa3bd340eed92a763b06
Hakan AKSOY <aksoyhakan1990gmail.com>
2023-01-27 12:43:33 +0300

    Tüm görevler yapılmıştır.

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,49 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import AppFunctional from "./AppFunctional";
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+const testText = "Koordinatlar (2-1)";
+
+test("Header Text Check", () => {
+  render(<AppFunctional />);
+  const info = screen.getByText(/0 kere ilerlediniz/i);
+  expect(info).toBeInTheDocument();
+  const left = screen.getByText("SOL");
+  fireEvent.click(left);
+  fireEvent.click(left);
+  expect(screen.getByText(/1 kere ilerlediniz/i)).toBeInTheDocument();
+});
+
+test("Coordinate Text Check", () => {
+  render(<AppFunctional />);
+  const info = screen.getByText(/Koordinatlar/i);
+  expect(info).toBeInTheDocument();
+  const left = screen.getByText("SOL");
+  fireEvent.click(left);
+  fireEvent.click(left);
+  expect(screen.getByText(/Koordinatlar/i).textContent).toBe(
+    "Koordinatlar (1-2)"
+  );
+});
+
+test("Button Check", () => {
+  render(<AppFunctional />);
+  const button = screen.getAllByRole("button");
+  expect(button.length).toBe(6);
+});
+
+test("Check B class", () => {
+  render(<AppFunctional />);
+  const bCell = screen.getByText(/B/i);
+  expect(bCell.className).toBe("square active");
+});
+
+test("Check e-mail input", () => {
+  render(<AppFunctional />);
+  const emailInput = screen.getByPlaceholderText("email girin");
+  fireEvent.change(emailInput, {
+    target: { value: "aksoyhakan" },
+  });
+  expect(screen.getByDisplayValue("aksoyhakan")).toBeInTheDocument();
+});
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,159 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+//  "B" nin bulunduğu indexi
+
+const dummyData = {
+  message: "",
+  email: "",
+  steps: 0,
+  index: 4,
+};
 
 export default function AppFunctional(props) {
+  const [data, setData] = useState(dummyData);
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+
+    return { x: (data.index % 3) + 1, y: Math.floor(data.index / 3) + 1 };
   }
 
   function getXYMesaj() {
     // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
     // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
     // tamamen oluşturulmuş stringi döndürür.
+
+    return `Koordinatlar (${getXY().x}-${getXY().y})`;
   }
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setData(dummyData);
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    let newIndex = data.index;
+
+    if (yon === "left") {
+      if (data.index % 3 !== 0) newIndex = data.index - 1;
+      else setData({ ...data, ["message"]: "Sola gidemezsiniz" });
+    }
+    if (yon === "right") {
+      if (data.index % 3 !== 2) newIndex = data.index + 1;
+      else setData({ ...data, ["message"]: "Sağa gidemezsiniz" });
+    }
+    if (yon === "up") {
+      if (Math.floor(data.index / 3) !== 0) newIndex = data.index - 3;
+      else setData({ ...data, ["message"]: "Yukarıya gidemezsiniz" });
+    }
+    if (yon === "down") {
+      if (Math.floor(data.index / 3) !== 2) newIndex = data.index + 3;
+      else setData({ ...data, ["message"]: "Aşağıya gidemezsiniz" });
+    }
+
+    return newIndex;
   }
 
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+
+    let newValue = sonrakiIndex(evt.target.id);
+    newValue !== data.index &&
+      setData({
+        ...data,
+        ["message"]: "",
+        ["steps"]: data.steps + 1,
+        ["index"]: newValue,
+      });
   }
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+    evt.target.id === "reset" && reset();
+    evt.target.id === "email" &&
+      setData({ ...data, ["email"]: evt.target.value });
+    evt.target.id !== "reset" && evt.target.id !== "email" && ilerle(evt);
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
+    let post = {
+      x: getXY().x,
+      y: getXY().y,
+      steps: data.steps,
+      email: data.email,
+    };
+
+    if (post.email === "") {
+      setData({ ...data, ["message"]: "Ouch: email is required" });
+    } else {
+      axios
+        .post("http://localhost:9000/api/result", post)
+        .then((result) => {
+          setData({ ...data, ["email"]: "", ["message"]: result.data.message });
+        })
+        .catch((err) =>
+          setData({ ...data, ["message"]: err.response.data.message })
+        );
+    }
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">{getXYMesaj()}</h3>
+        <h3 id="steps">{data.steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === data.index ? " active" : ""}`}
+          >
+            {idx === data.index ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{data.message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={onChange} id="left">
+          SOL
+        </button>
+        <button onClick={onChange} id="up">
+          YUKARI
+        </button>
+        <button onClick={onChange} id="right">
+          SAĞ
+        </button>
+        <button onClick={onChange} id="down">
+          AŞAĞI
+        </button>
+        <button onClick={onChange} id="reset">
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          id="email"
+          type="email"
+          placeholder="email girin"
+          value={data.email}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
